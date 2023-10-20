f2dc92427d26a53f083f94e0d3710f2df160e3b6
testpravigit <139649757+testpravigit@users.noreply.github.com>
2023-10-21 00:59:09 +0530

    Add files via upload

---
+++ port.js
@@ -0,0 +1,83 @@
+var b1h=document.getElementById('b1h');
+var b1p=document.getElementById('b1p');
+var b1=document.getElementById('b1');
+b1.addEventListener('mouseover',(event)=>{
+  b1h.innerHTML="Weather Application";
+  b1p.innerHTML="Using  API in javascript and user location it will give the weather information."
+
+  
+})
+b1.addEventListener('mouseout',(event)=>{
+    b1h.innerHTML=""
+    b1p.innerHTML=""
+  })
+
+  var b2h=document.getElementById('b2h');
+var b2p=document.getElementById('b2p');
+var b2=document.getElementById('b2');
+b2.addEventListener('mouseover',(event)=>{
+    b2h.innerHTML="Quizz App";
+    b2p.innerHTML="Using javascript buid one quizz app based on the user input it will display the output."
+  
+    
+  })
+  b2.addEventListener('mouseout',(event)=>{
+      b2h.innerHTML=""
+      b2p.innerHTML=""
+    })
+
+    var b3h=document.getElementById('b3h');
+    var b3p=document.getElementById('b3p');
+    var b3=document.getElementById('b3');
+    b3.addEventListener('mouseover',(event)=>{
+        b3h.innerHTML="Form validation";
+        b3p.innerHTML="Using javascript and html&css design form page after make validation."
+      
+        
+      })
+      b3.addEventListener('mouseout',(event)=>{
+          b3h.innerHTML=""
+          b3p.innerHTML=""
+        })
+
+        var b4h=document.getElementById('b4h');
+        var b4p=document.getElementById('b4p');
+        var b4=document.getElementById('b4');
+        b4.addEventListener('mouseover',(event)=>{
+            b4h.innerHTML="Time countdown";
+            b4p.innerHTML=" Using javasccript build one countdown application."
+          
+            
+          })
+          b4.addEventListener('mouseout',(event)=>{
+              b4h.innerHTML=""
+              b4p.innerHTML=""
+            })
+
+            var b5h=document.getElementById('b5h');
+            var b5p=document.getElementById('b5p');
+            var b5=document.getElementById('b5');
+            b5.addEventListener('mouseover',(event)=>{
+                b5h.innerHTML="Electricity generation";
+                b5p.innerHTML=" Using plastic generate electricity."
+              
+                
+              })
+              b5.addEventListener('mouseout',(event)=>{
+                  b5h.innerHTML=""
+                  b5p.innerHTML=""
+                })
+
+                var b6h=document.getElementById('b6h');
+                var b6p=document.getElementById('b6p');
+                var b6=document.getElementById('b6');
+                b6.addEventListener('mouseover',(event)=>{
+                    b6h.innerHTML="Design web pages";
+                    b6p.innerHTML="Using html and css create some front pages. "
+                  
+                    
+                  })
+                  b6.addEventListener('mouseout',(event)=>{
+                      b6h.innerHTML=""
+                      b6p.innerHTML=""
+                    })
